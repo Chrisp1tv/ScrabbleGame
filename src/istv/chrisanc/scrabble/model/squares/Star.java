4fdcc4acc429960ca7867a62657d153a6028aec2
Christopher Anciaux <christopher.anciaux@gmail.com>
2017-09-11 18:03:29 +0200

    Added Star Square (the center square)

---
+++ src/istv/chrisanc/scrabble/model/squares/Star.java
@@ -0,0 +1,12 @@
+package istv.chrisanc.scrabble.model.squares;
+
+import istv.chrisanc.scrabble.model.Square;
+
+/**
+ * This class represents the center square of the Scrabble board, the star.
+ *
+ * @author Christopher Anciaux
+ */
+public class Star extends Square {
+    protected byte wordMultiplier = 2;
+}
