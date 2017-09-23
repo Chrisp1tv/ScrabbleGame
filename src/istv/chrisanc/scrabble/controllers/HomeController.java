0afc37ee1ead6616feadda67bc021d705a59112d
Christopher Anciaux <christopher.anciaux@gmail.com>
2017-09-23 14:47:30 +0200

    Prepared the development of the UI by creating the Root view, modifying the Root controller and dividing the tasks between UI developers

--- src/istv/chrisanc/scrabble/controllers/HomeController.java
+++ src/istv/chrisanc/scrabble/controllers/HomeController.java
@@ -1,7 +1,35 @@
 package istv.chrisanc.scrabble.controllers;
 
+import javafx.fxml.FXML;
+
 /**
+ * This controller manages the Home of the Scrabble, the first page displayed to the user, allowing him to choose between
+ * a new game, loading one, or quitting the game.
+ *
  * @author Christopher Anciaux
  */
-public class HomeController {
+public class HomeController extends BaseController {
+    /**
+     * Redirects to the NewGame action
+     */
+    @FXML
+    protected void handleNewGame() {
+        this.scrabble.showNewGame();
+    }
+
+    /**
+     * Redirects to the LoadGame action
+     */
+    @FXML
+    protected void handleLoadGame() {
+        this.scrabble.showLoadGame();
+    }
+
+    /**
+     * Closes the application.
+     */
+    @FXML
+    protected void handleExit() {
+        System.exit(0);
+    }
 }
--- src/istv/chrisanc/scrabble/controllers/RootLayoutController.java
+++ src/istv/chrisanc/scrabble/controllers/RootLayoutController.java
@@ -1,7 +1,32 @@
 package istv.chrisanc.scrabble.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+
 /**
+ * This controller manages the root layout of the Application, that is to say the About action, and the main graphic container.
+ *
  * @author Christopher Anciaux
  */
-public class RootLayoutController {
+public class RootLayoutController extends BaseController {
+    /**
+     * Opens an about dialog.
+     */
+    @FXML
+    protected void handleAbout() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(this.scrabble.getI18nMessages().getString("Scrabble"));
+        alert.setHeaderText(this.scrabble.getI18nMessages().getString("aboutHeaderText"));
+        alert.setContentText(this.scrabble.getI18nMessages().getString("aboutContentText"));
+
+        alert.showAndWait();
+    }
+
+    /**
+     * Closes the application.
+     */
+    @FXML
+    protected void handleExit() {
+        System.exit(0);
+    }
 }
