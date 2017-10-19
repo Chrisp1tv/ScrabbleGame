e0b3484c606df2842e267fa38d91e323f974ff38
Xoreas <31727223+Xoreas@users.noreply.github.com>
2017-10-19 10:50:42 +0200

    Debug and added asked function
    Solved a problem where the multiplier were disabled after the first
    word.
    Added a function that let you know the score of a play without disabling
    the multipliers

--- src/istv/chrisanc/scrabble/utils/ScoreManager.java
+++ src/istv/chrisanc/scrabble/utils/ScoreManager.java
@@ -1,12 +1,9 @@
 package istv.chrisanc.scrabble.utils;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import istv.chrisanc.scrabble.model.Player;
 import istv.chrisanc.scrabble.model.interfaces.BoardInterface;
 import istv.chrisanc.scrabble.model.interfaces.WordInterface;
-import javafx.collections.ObservableList;
 
 /**
  * This class handles all the calculations of the {@link Player}'s score, and manages all score-relative logic.
@@ -33,9 +30,6 @@ public class ScoreManager {
     					wordMulti *= board.getSquares()
     								.get(playedWords.get(i).getStartColumn()+j)
     								.get(playedWords.get(i).getStartLine()).getWordMultiplier();
-    					board.getSquares()
-							.get(playedWords.get(i).getStartColumn()+j)
-							.get(playedWords.get(i).getStartLine()).makeMultiplierUsed();
     				}else{//Multiplier used
     					Score += ((int) playedWords.get(i).getLetters().get(j).getValue());
     				}
@@ -51,9 +45,6 @@ public class ScoreManager {
         					wordMulti *= board.getSquares()
         								.get(playedWords.get(i).getStartColumn())
         								.get(playedWords.get(i).getStartLine()+j).getWordMultiplier();
-        					board.getSquares()
-    							.get(playedWords.get(i).getStartColumn())
-    							.get(playedWords.get(i).getStartLine()+j).makeMultiplierUsed();
         				}else{//Multiplier used
         					Score += ((int) playedWords.get(i).getLetters().get(j).getValue());
     				}
@@ -61,10 +52,104 @@ public class ScoreManager {
     		}
     		Somme += Score*wordMulti;
     	}
+    	int nb=0;
+    	for(i=0;i<playedWords.size();i++){
+    		for(j=0;j<playedWords.get(i).getLetters().size();j++){
+    			if(playedWords.get(i).isHorizontal()){
+    				if(board.getSquares()
+    						.get(playedWords.get(i).getStartColumn()+j)
+    						.get(playedWords.get(i).getStartLine()).isMultiplierUsed()==false)
+        				{
+    						nb+=1;
+    						board.getSquares()
+    							.get(playedWords.get(i).getStartColumn()+j)
+    							.get(playedWords.get(i).getStartLine()).makeMultiplierUsed();
+        				}
+    			}else{
+    				if(board.getSquares()
+    						.get(playedWords.get(i).getStartColumn())
+    						.get(playedWords.get(i).getStartLine()+j).isMultiplierUsed()==false)
+        			{
+    					nb+=1;
+    					board.getSquares()
+							.get(playedWords.get(i).getStartColumn())
+							.get(playedWords.get(i).getStartLine()+j).makeMultiplierUsed();
+        			}
+    			}
+    		}
+    	}
+    	if(nb==7){
+    		Somme+=50;
+    	}
     	return Somme;
-        /* TODO
-        This method should actualize all the Square states :
-        For each letter placed on the square, the MultiplierUsed value of the square must be changed to true
-        */
+    }
+    		/*
+    		*	that function is a specific ask from the IA's programming team to compare the best moves to do 
+    		*	this is like the updateScore function but it let the multipliers unchanged
+    		*/
+    public static int testScore(List<WordInterface> playedWords, BoardInterface board) {
+    	int i,j,Somme=0;
+    	for(i=0;i<playedWords.size();i++){
+    		int wordMulti=1;
+    		int Score=0;
+    		for(j=0;j<playedWords.get(i).getLetters().size();j++){
+    			if(playedWords.get(i).isHorizontal()){
+    				if(board.getSquares()
+						.get(playedWords.get(i).getStartColumn()+j)
+						.get(playedWords.get(i).getStartLine()).isMultiplierUsed()==false)
+    				{	//Multiplier unused and Horizontal Word
+    					Score += ((int) playedWords.get(i).getLetters().get(j).getValue())
+    								*board.getSquares()
+    								.get(playedWords.get(i).getStartColumn()+j)
+    								.get(playedWords.get(i).getStartLine()).getLetterMultiplier();
+    					wordMulti *= board.getSquares()
+    								.get(playedWords.get(i).getStartColumn()+j)
+    								.get(playedWords.get(i).getStartLine()).getWordMultiplier();
+    				}else{//Multiplier used
+    					Score += ((int) playedWords.get(i).getLetters().get(j).getValue());
+    				}
+    			}else{	
+    				if(board.getSquares()
+    						.get(playedWords.get(i).getStartColumn())
+    						.get(playedWords.get(i).getStartLine()+j).isMultiplierUsed()==false)
+        				{	//Multiplier unused and Vertical word
+    						Score += ((int) playedWords.get(i).getLetters().get(j).getValue())
+    									*board.getSquares()
+    									.get(playedWords.get(i).getStartColumn())
+    									.get(playedWords.get(i).getStartLine()+j).getLetterMultiplier();
+        					wordMulti *= board.getSquares()
+        								.get(playedWords.get(i).getStartColumn())
+        								.get(playedWords.get(i).getStartLine()+j).getWordMultiplier();
+        				}else{//Multiplier used
+        					Score += ((int) playedWords.get(i).getLetters().get(j).getValue());
+    				}
+    			}
+    		}
+    		Somme += Score*wordMulti;
+    	}
+    	int nb=0;
+    	for(i=0;i<playedWords.size();i++){
+    		for(j=0;j<playedWords.get(i).getLetters().size();j++){
+    			if(playedWords.get(i).isHorizontal()){
+    				if(board.getSquares()
+    						.get(playedWords.get(i).getStartColumn()+j)
+    						.get(playedWords.get(i).getStartLine()).isMultiplierUsed()==false)
+        			{
+    					nb+=1;
+    				}
+    			}else{
+    				if(board.getSquares()
+    						.get(playedWords.get(i).getStartColumn())
+    						.get(playedWords.get(i).getStartLine()+j).isMultiplierUsed()==false)
+        			{
+    					nb+=1;
+    				}
+    			}
+    		}
+    	}
+    	if(nb==7){
+    		Somme+=50;
+    	}
+    	return Somme;
     }
 }
