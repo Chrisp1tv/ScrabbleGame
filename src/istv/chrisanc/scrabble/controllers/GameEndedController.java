7df5cadce88d229422543fdd4cbf91bee7b02a50
Abd3L GFX <Abd3L GFX@Abd3L-Computer>
2017-10-24 14:10:35 +0200

    ShowWinner Done.

--- src/istv/chrisanc/scrabble/controllers/GameEndedController.java
+++ src/istv/chrisanc/scrabble/controllers/GameEndedController.java
@@ -1,5 +1,12 @@
 package istv.chrisanc.scrabble.controllers;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import istv.chrisanc.scrabble.model.interfaces.PlayerInterface;
 import javafx.fxml.FXML;
 
 /**
@@ -19,7 +26,7 @@ public class GameEndedController extends BaseController {
 	protected void handleNewGame() {
 		this.scrabble.showNewGame();
 	}
-	
+
 
 	@FXML
 	protected void handleQuit() {
@@ -27,6 +34,47 @@ public class GameEndedController extends BaseController {
 	}
 
 	protected void showWinner() {
+
+		//int temp=0;
 		//ObservableList<Person> personData = FXCollections.observableArrayList();
+		//ArrayList<Integer> totalPoints = new ArrayList<>();
+		List<PlayerInterface> players = this.scrabble.getPlayers();
+
+		/**
+		// Get Score Of Each Player
+		for(PlayerInterface p : players) {
+			totalPoints.add(p.getScore());
+		}
+
+		//Sort Players
+		Collections.sort(totalPoints);
+		ArrayList<Integer> totalPointsSorted = new ArrayList<>();
+
+		for(int i=totalPoints.size()-1;i>=0;i--) {
+			totalPointsSorted.add(totalPoints.get(i));
+		}
+
+		System.out.println("The Winner Is ");
+		**/
+
+
+		Collections.sort(players,new Comparator<PlayerInterface>() {
+
+			@Override
+			public int compare(PlayerInterface p1, PlayerInterface p2) {
+				if(p1.getScore()>p2.getScore()) {
+					return 1;
+				} else if(p1.getScore()<p2.getScore()) {
+					return -1;
+				} else {
+					return 0;
+				}
+			}
+
+		});
+
+		System.out.println("The Winner Is >> "+players.get(players.size()-1).getName());
+
+
 	}
 }
