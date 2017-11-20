36d650a52f3b16400d2891019bfba8d65b0b4657
Xoreas <31727223+Xoreas@users.noreply.github.com>
2017-11-20 10:30:46 +0100

    Added help notion to the player.
    function help to get the most valuable word the player can play.
    function getHelp to know how much helps the player has
    function decreaseHelp to reduce numbers of help by 1.

---
+++ src/istv/chrisanc/scrabble/exceptions/utils/LetterToStringTransformationException.java
@@ -0,0 +1,10 @@
+package istv.chrisanc.scrabble.exceptions.utils;
+
+/**
+ * @author Christopher Anciaux
+ */
+public class LetterToStringTransformationException extends Exception {
+    public LetterToStringTransformationException() {
+        super("exceptions.utils.letterToStringTransformer");
+    }
+}
